d7238ef2d37087fde098cc4e8a0f1b45cec4e3a1
sunbaocai <63283447+sunbaocai@users.noreply.github.com>
2020-06-13 21:25:38 +0800

    哈夫曼

---
+++ 数据结构试验/第六章 树和二叉树/HuffmanTree工程（DevC++）/HuffmanTree-main.cpp
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include "HuffmanTree.h"     //**▲实验三 树和二叉树**//
+
+int main(int argc, char* argv[]) {
+    int *w;
+    int n;
+    HuffmanTree HT;
+    HuffmanCode HC;
+    
+    printf("████████ InitEnvironment \n");
+    {
+        printf("█ 初始化环境，主要是初始化权值信息...\n");
+        InitEnvironment(&w, &n, NULL);
+    }
+    PressEnterToContinue(debug);
+    
+    
+    printf("████████ HuffmanCodeing \n");
+    {
+        printf("█ 编码：由权值信息获取赫夫曼编码...\n");
+        HuffmanCodeing_2(&HT, &HC, w, n);   // 算法6.13
+        
+        printf("█ 打印赫夫曼树结构...\n");
+        PrintHuffmanTree(HT);
+    
+        printf("█ 打印赫夫曼编码...\n");
+        PrintHuffmanCode(HT, HC);
+    }
+    PressEnterToContinue(debug);
+    
+    
+    printf("████████ HuffmanDecoding \n");
+    {
+        int* weight;
+        
+        printf("█ 解码：由赫夫曼编码获取权值信息...\n");
+        HuffmanDecoding(HT,HC,&weight,n);
+    
+        printf("█ 打印权值信息...\n");
+        PrintWeight(HC, weight, n);
+    }
+    PressEnterToContinue(debug);
+    
+    return OK;
+}
